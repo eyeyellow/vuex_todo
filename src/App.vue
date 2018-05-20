1fc45c3da0624db81c79c6d87d4e09e5e01fbcf0
Frank Aiello <frankvaiello@gmail.com>
2018-05-20 14:59:45 -0400

    Sets up app title component and style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Title msg="ToDo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Title
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 10% auto;
+  width: 800px;
+  height: 800px;
+  border: 2px dotted black;
 }
 </style>
---
+++ src/components/Title.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="title">
+    <h1 id="header">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Title',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<style>
+#title {
+  border-bottom: 2px dotted black;
+  text-align: center;
+}
+h1#header {
+  font-family: Helvetica, sans-serif;
+}
+</style>
