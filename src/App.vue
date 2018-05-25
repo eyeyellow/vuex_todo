2dac374040f4ed570a0b6a92704404c7833714c1
Frank Aiello <frankvaiello@gmail.com>
2018-05-25 07:48:24 -0400

    Implements basic counter in vuex

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
     <Title msg="ToDo"/>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    {{ count }}
   </div>
 </template>
 
 <script>
-import Title from './components/Title.vue'
+import Title from './components/Title.vue';
+import store from './store/store';
 
 export default {
   name: 'app',
   components: {
     Title
+  },
+  store,
+  methods: {
+    increment () {
+      store.commit('increment');
+    },
+    decrement () {
+      store.commit('decrement');
+    }
+  },
+  computed: {
+    count () {
+      return store.state.count;
+    }
   }
 }
 </script>
